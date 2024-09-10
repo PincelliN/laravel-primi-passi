e8a4e148517e81e46d5b6237b9839a81b339d27d
Nicolò Pincelli <pincelli.n@gmail.com>
2024-09-10 08:38:28 +0200

    Add card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="/../css/home.css">
 
     <title>Prima pagina con Laravel</title>
 </head>
@@ -34,21 +35,34 @@
                     </div>
                 </div>
             </nav>
-            <h1>{{ $mess }}</h1>
-
-            <p>Ciao {{ $user['name'] }} {{ $user['lastname'] }} tanti auguri per i sui {{ $user['age'] }} anni</p>
-
 
 
             @empty($programmings_language)
                 <h3>Devo mettermi ha studiare</h3>
             @else
-                <h3>Linguaggi di programmazione conosciuti</h3>
-                <ul class="list-group">
-                    @foreach ($programmings_language as $programming_language)
-                        <li class="list-group-item">{{ $programming_language }}</li>
-                    @endforeach
-                </ul>
+                <div class="card my-4 "
+                    style="width: 18rem; background: rgb(34,193,195);background: linear-gradient(0deg, rgba(34,193,195,1) 0%, rgba(63,244,6,0.8071603641456583) 100%);">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $mess }}</h5>
+                        <h6>Informazione personali</h6>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Nome:{{ $user['name'] }}</li>
+                            <li class="list-group-item">Cognome:{{ $user['lastname'] }}</li>
+                            <li class="list-group-item">Età:{{ $user['age'] }}</li>
+                    </div>
+                    <h6>Linguaggi di programmazione conosciuti</h6>
+                    <ul class="list-group list-group-flush mb-2">
+                        @foreach ($programmings_language as $programming_language)
+                            @if ($loop->first)
+                                <li class="list-group-item active">{{ $programming_language }}</li>
+                            @else
+                                <li class="list-group-item">{{ $programming_language }}</li>
+                            @endif
+                        @endforeach
+                    </ul>
+
+                </div>
+
             @endempty
 
         </div>
